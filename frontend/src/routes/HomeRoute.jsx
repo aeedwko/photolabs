ae89a0a483850918a5c5c7fae65a723d0542d84f
Edwin Ko <edwin.mk.ko@gmail.com>
2024-03-10 14:58:51 -0700

    move HomeRoute to correct routes folder

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import '../styles/HomeRoute.scss';
+import TopNavigationBar from "../components/TopNavigationBar";
+import PhotoList from "../components/PhotoList";
 
-const HomeRoute = () => {
+import "../styles/HomeRoute.scss";
+
+const HomeRoute = ({ photos, topics }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  const modifyFavourites = (id) => {
+    
+    // setFavourites((prevFavourites) => {
+    //   // if photo is not in the prevFavourite state
+    //   if (!prevFavourites.includes(id)) {
+    //     // return new state array with the id
+    //     console.log([...prevFavourites, id]);
+    //     return [...prevFavourites, id];
+    //   } else {
+    //     // return new state array without the id
+    //     console.log([prevFavourites.filter((element) => element !== id)]);
+    //     return prevFavourites.filter((element) => element !== id);
+    //   }
+    // });
+    
+    // if id is in favourites, then set favourites without id
+    favourites.includes(id) ? setFavourites((favourites.filter(favouriteId => favouriteId !== id))) : setFavourites([...favourites, id]);
+  };
+
+  // console.log(favourites);
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar topics={topics} favouritesExist={favourites.length > 0}/>
+      <PhotoList photos={photos} favourites={favourites} modifyFavourites={modifyFavourites} />
     </div>
   );
 };
